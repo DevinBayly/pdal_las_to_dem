7b95ed983dc62e037469936d10792fc49b64e6ca
DevinBayly <baylyd@email.arizona.edu>
2022-02-14 14:01:56 -0700

    first commit

---
+++ pdal_las_to_dem.py
@@ -0,0 +1,33 @@
+import pdal
+from pathlib import Path
+import multiprocessing as mp
+
+
+def process(fl,i):
+    pj = f'''[
+          "{fl}",
+        {
+            "filename":"./dtm{i}_{fl.name}.tif",
+            "gdaldriver":"GTiff",
+            "output_type":"all",
+            "resolution":"2.0",
+            "type": "writers.gdal"
+        }
+        
+
+    ]'''
+    pipeline = pdal.Pipeline(pj)
+
+    count = pipeline.execute()
+    arrays = pipeline.arrays
+    metadata = pipeline.metadata
+    log = pipeline.log
+
+las_files = list(Path("/data/input/files/public/LIDAR/2015").glob("**/*.las"))
+
+with mp.Pool(processes = 6) as pool:
+    procs = [pool.apply_async(process,(fl,i,)) for i,fl in enumerate(las_files)]
+    [res.wait() for res in procs]
+
+print("done!")
+
