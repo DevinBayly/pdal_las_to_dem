4e5bc006ba0e29d2ef6e6b15b9c5c17c19cfe96f
DevinBayly <baylyd@email.arizona.edu>
2022-02-14 15:14:58 -0700

    fix formatting issue

--- pdal_las_to_dem.py
+++ pdal_las_to_dem.py
@@ -5,24 +5,25 @@ import time
 
 
 def process(fl,i):
-    pj = f'''[
-          "{fl}",
-        {
-            "filename":"./dtm{i}_{fl.name}.tif",
-            "gdaldriver":"GTiff",
-            "output_type":"all",
-            "resolution":"2.0",
-            "type": "writers.gdal"
-        }
-        
+pj =
+f"""
+[
+      '{fl}',
+    {{
+        'filename':'./dtm{i}_{fl.name}.tif',
+        'gdaldriver':'GTiff',
+        'output_type':'all',
+        'resolution':'2.0',
+        'type': 'writers.gdal'
+    }}
+]"""
+    print(pj)
+    ##pipeline = pdal.Pipeline(pj)
 
-    ]'''
-    pipeline = pdal.Pipeline(pj)
-
-    count = pipeline.execute()
-    arrays = pipeline.arrays
-    metadata = pipeline.metadata
-    log = pipeline.log
+    ##count = pipeline.execute()
+    ##arrays = pipeline.arrays
+    ##metadata = pipeline.metadata
+    ##log = pipeline.log
 
 las_files = list(Path("/data/input/files/LIDAR/2015").glob("**/*.las"))[:6]
 
@@ -34,6 +35,7 @@ def runTest(i):
 with mp.Pool(processes = 6) as pool:
     procs = [pool.apply_async(process,(fl,i)) for i,fl in enumerate(las_files)]
     [res.wait() for res in procs]
+    [res.get() for res in procs]
 
 print("done!")
 
