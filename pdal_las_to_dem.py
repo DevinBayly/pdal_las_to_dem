1b11f82af97e1fa5244db877a8d1299ea2ba8459
Devin Bayly <baylyd@email.arizona.edu>
2022-02-14 15:43:36 -0700

    Update pdal_las_to_dem.py

--- pdal_las_to_dem.py
+++ pdal_las_to_dem.py
@@ -5,33 +5,32 @@ import time
 
 
 def process(fl,i):
-    pj =f"""
-      [
-            '{fl}',
-          {{
-              'filename':'./dtm{i}_{fl.name}.tif',
-              'gdaldriver':'GTiff',
-              'output_type':'all',
-              'resolution':'2.0',
-              'type': 'writers.gdal'
-          }}
-    ]"""
+    pj =f'''[
+      "{fl}",
+    {{
+        "filename":"./dtm{i}_{fl.name}.tif",
+        "gdaldriver":"GTiff",
+        "output_type":"all",
+        "resolution":"2.0",
+        "type": "writers.gdal"
+    }}
+]'''
+
     print(pj)
-    ##pipeline = pdal.Pipeline(pj)
+    pipeline = pdal.Pipeline(pj)
+    count = pipeline.execute()
+    arrays = pipeline.arrays
+    etadata = pipeline.metadata
+    log = pipeline.log
 
-    ##count = pipeline.execute()
-    ##arrays = pipeline.arrays
-    ##metadata = pipeline.metadata
-    ##log = pipeline.log
-
-las_files = list(Path("/data/input/files/LIDAR/2015").glob("**/*.las"))[:6]
+las_files = list(Path("/data/input/files/LIDAR/2015").glob("**/*.las"))
 
 def runTest(i):
     print("starting")
     time.sleep(i)
     print(i)
 
-with mp.Pool(processes = 6) as pool:
+with mp.Pool(processes = mp.cpu_count()) as pool:
     procs = [pool.apply_async(process,(fl,i)) for i,fl in enumerate(las_files)]
     [res.wait() for res in procs]
     [res.get() for res in procs]
